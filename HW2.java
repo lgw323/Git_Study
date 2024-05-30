42b8042039ce296b309735aa55c4036a9c2c102d
LGW <dlrjsdn0518.gmail.com>
2024-05-30 08:25:13 +0000

    HW2 과제 작성

---
+++ HW2.java
@@ -0,0 +1,10 @@
+public class HW2 {
+    public static void main(String[] args)
+    {
+        int a = 0;
+        int b = 10;
+        int c = 20;
+        a = b + c;
+        System.out.println(a);
+    }
+}
