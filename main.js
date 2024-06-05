39d22ad361e9b685503fdcbb5691479d0139f9fb
LGW <dlrjsdn0518.gmail.com>
2024-06-05 09:09:02 +0000

    -0605 이건우 로그인 기능 구현

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ app.listen(3000, ()=>{ // 서버 생성, 포트번호:3000
 app.get('/',(req,res)=>{
     console.log('루트에 대한 요청');
     res.send('루트페이지에 대한 요청');
-}) 
+})
 
 app.get('/test',(req,res)=>{
     console.log('===> test에 대한 요청');
@@ -18,4 +18,8 @@ app.get('/test',(req,res)=>{
 app.get('/lgw323',(req,res)=>{
     console.log('===> lgw323에 대한 요청');
     res.send('lgw323에 대한 요청');
+})
+app.get('/login',(req,res)=>{
+    console.log('===> 로그인 요청');
+    res.send('로그인 페이지 예시');
 })
